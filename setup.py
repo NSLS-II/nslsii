0fe9febbacaf81844b7bbf1830a952b447477f78
Stuart B. Wilkins <swilkins@bnl.gov>
2017-08-20 09:37:14 -0400

    BLD: Cannot use '-' in names!

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import versioneer
 import setuptools
 
 setuptools.setup(
-	name='nsls-ii-tools',
+	name='nsls2tools',
     version=versioneer.get_version(),
 	cmdclass=versioneer.get_cmdclass(),
     license="BSD (3-clause)",
