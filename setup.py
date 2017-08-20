709f34974256aadd126b4942be0b4d1965d8779a
Stuart B. Wilkins <swilkins@bnl.gov>
2017-08-20 08:58:19 -0400

    BLD: Added setup scripts

---
+++ setup.py
@@ -0,0 +1,15 @@
+from __future__ import (absolute_import, division, print_function)
+
+import versioneer
+import setuptools
+
+setuptools.setup(
+	name='nsls-ii-tools',
+    version=versioneer.get_version(),
+	cmdclass=versioneer.get_cmdclass(),
+    license="BSD (3-clause)",
+    packages=setuptools.find_packages(),
+    description='Tools for data collection and analysis at NSLS-II',
+	author='Stuart B. Wilkins',
+    author_email='swilkins@bnl.gov',
+)
