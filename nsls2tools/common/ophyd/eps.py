ef7123e76f68e1860a1479c920b54da864e5d3d8
Stuart B. Wilkins <swilkins@bnl.gov>
2017-09-25 13:53:20 -0400

    Added EPS shutter / valve

---
+++ nsls2tools/common/ophyd/eps.py
@@ -0,0 +1,69 @@
+from ophyd.device import Device
+
+class EPSOpenCloseDevice(Device):
+	# @tcaswell, the names don't need to be fixed. These commands run
+    # when the record is processed, you could as easily poke .PROC
+    open_cmd = Cpt(EpicsSignal, 'Cmd:Opn-Cmd', string=True)
+    open_val = 'Open'
+
+    close_cmd = Cpt(EpicsSignal, 'Cmd:Cls-Cmd', string=True)
+    close_val = 'Not Open'
+
+    status = Cpt(EpicsSignalRO, 'Pos-Sts', string=True)
+    fail_to_close = Cpt(EpicsSignalRO, 'Sts:FailCls-Sts', string=True)
+    fail_to_open = Cpt(EpicsSignalRO, 'Sts:FailOpn-Sts', string=True)
+    # user facing commands
+    open_str = 'Open'
+    close_str = 'Close'
+
+    def set(self, val):
+        if self._set_st is not None:
+            raise RuntimeError('trying to set while a set is in progress')
+
+        cmd_map = {self.open_str: self.open_cmd,
+                   self.close_str: self.close_cmd}
+        target_map = {self.open_str: self.open_val,
+                      self.close_str: self.close_val}
+
+        cmd_sig = cmd_map[val]
+        target_val = target_map[val]
+
+        st = self._set_st = DeviceStatus(self)
+        enums = self.status.enum_strs
+
+        def shutter_cb(value, timestamp, **kwargs):
+            value = enums[int(value)]
+            if value == target_val:
+                self._set_st._finished()
+                self._set_st = None
+                self.status.clear_sub(shutter_cb)
+
+        cmd_enums = cmd_sig.enum_strs
+        count = 0
+        def cmd_retry_cb(value, timestamp, **kwargs):
+            nonlocal count
+            value = cmd_enums[int(value)]
+            count += 1
+            if count > 5:
+                cmd_sig.clear_sub(cmd_retry_cb)
+                st._finished(success=False)
+            if value == 'None':
+                if not st.done:
+                    time.sleep(.5)
+                    cmd_sig.set(1)
+                    ts = datetime.datetime.fromtimestamp(timestamp).strftime(_time_fmtstr)
+                    print('** ({}) Had to reactuate shutter while {}ing'.format(ts, val))
+                else:
+                    cmd_sig.clear_sub(cmd_retry_cb)
+
+        cmd_sig.subscribe(cmd_retry_cb, run=False)
+        cmd_sig.set(1)
+        self.status.subscribe(shutter_cb)
+
+        return st
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._set_st = None
+        self.read_attrs = ['status']
+
