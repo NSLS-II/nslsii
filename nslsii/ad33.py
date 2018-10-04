74261934e3df0debfcd4b8691015107412b29e8b
Maksim Rakitin <maxim.rakitin@gmail.com>
2018-10-04 13:41:01 -0400

    ENH: add QuadEMV33 class with StatsPluginV33

--- nslsii/ad33.py
+++ nslsii/ad33.py
@@ -13,8 +13,14 @@ from ophyd.areadetector.base import (ADComponent as C, ad_group,
 from ophyd.areadetector.plugins import PluginBase
 from ophyd.areadetector.trigger_mixins import TriggerBase, ADTriggerStatus
 from ophyd.device import DynamicDeviceComponent as DDC, Staged
-from ophyd.signal import (EpicsSignalRO, EpicsSignal)
+from ophyd.signal import (EpicsSignalRO, EpicsSignal, Signal)
 
+from ophyd import (Kind, kind_context)
+from ophyd.areadetector import (ADComponent as ADCpt, EpicsSignalWithRBV,
+                                ImagePlugin, StatsPlugin, DetectorBase,
+                                SingleTrigger, ADBase)
+from ophyd.status import DeviceStatus
+from ophyd import QuadEMPort, _current_fields
 
 import time as ttime
 
@@ -221,3 +227,91 @@ class StatsPluginV33(PluginBase):
     ts_sigma_y = C(EpicsSignal, 'TS:TSSigmaY')
     ts_total = C(EpicsSignal, 'TS:TSTotal')
     total = C(EpicsSignalRO, 'Total_RBV')
+
+
+class QuadEMV33(SingleTrigger, DetectorBase):
+    # These settings intentionally shadow the settings inherited from
+    # DetectorBase.
+    _default_read_attrs = None
+    _default_configuration_attrs = None
+
+    _status_type = DeviceStatus  # overrriding the default in SingleTrigger
+
+    # This is needed because ophyd verifies that it can see all
+    # of the nodes in the asyn pipeline, however these IOCs do not
+    # expose their port name via a PV, but nevertheless server as the
+    # root node for the plugins.
+    # Leaving this port_name here for compatibility
+    integration_time = Cpt(EpicsSignalWithRBV, 'IntegrationTime',
+                           kind='config')
+    averaging_time = Cpt(EpicsSignalWithRBV, 'AveragingTime', kind='config')
+    with kind_context('omitted') as OCpt:
+        conf = Cpt(QuadEMPort, port_name='NSLS_EM')
+        model = OCpt(EpicsSignalRO, 'Model')
+        firmware = OCpt(EpicsSignalRO, 'Firmware')
+
+        acquire_mode = OCpt(EpicsSignalWithRBV, 'AcquireMode')
+        acquire = OCpt(EpicsSignal, 'Acquire')
+
+        read_format = OCpt(EpicsSignalWithRBV, 'ReadFormat')
+        em_range = OCpt(EpicsSignalWithRBV, 'Range')
+        ping_pong = OCpt(EpicsSignalWithRBV, 'PingPong')
+
+        num_channels = OCpt(EpicsSignalWithRBV, 'NumChannels')
+        geometry = OCpt(EpicsSignalWithRBV, 'Geometry')
+        resolution = OCpt(EpicsSignalWithRBV, 'Resolution')
+
+        bias_state = OCpt(EpicsSignalWithRBV, 'BiasState')
+        bias_interlock = OCpt(EpicsSignalWithRBV, 'BiasInterlock')
+        bias_voltage = OCpt(EpicsSignalWithRBV, 'BiasVoltage')
+        hvs_readback = OCpt(EpicsSignalRO, 'HVSReadback')
+        hvv_readback = OCpt(EpicsSignalRO, 'HVVReadback')
+        hvi_readback = OCpt(EpicsSignalRO, 'HVIReadback')
+
+        values_per_read = OCpt(EpicsSignalWithRBV, 'ValuesPerRead')
+        sample_time = OCpt(EpicsSignalRO, 'SampleTime_RBV')  # yay consistency
+        num_average = OCpt(EpicsSignalRO, 'NumAverage_RBV')
+        num_averaged = OCpt(EpicsSignalRO, 'NumAveraged_RBV')
+        num_acquire = OCpt(EpicsSignalWithRBV, 'NumAcquire')
+        num_acquired = OCpt(EpicsSignalRO, 'NumAcquired')
+        read_data = OCpt(EpicsSignalRO, 'ReadData')
+        ring_overflows = OCpt(EpicsSignalRO, 'RingOverflows')
+        trigger_mode = OCpt(EpicsSignal, 'TriggerMode')
+        reset = OCpt(EpicsSignal, 'Reset')
+
+    current_names = DDCpt(_current_fields('ch', 'CurrentName', range(1, 5),
+                                          string=True))
+    current_offsets = DDCpt(_current_fields('ch', 'CurrentOffset',
+                                            range(1, 5)))
+    current_offset_calcs = DDCpt(_current_fields('ch', 'ComputeCurrentOffset',
+                                                 range(1, 5)))
+    current_scales = DDCpt(_current_fields('ch', 'CurrentScale', range(1, 5)))
+
+    position_offset_x = Cpt(EpicsSignal, 'PositionOffsetX')
+    position_offset_y = Cpt(EpicsSignal, 'PositionOffsetY')
+
+    position_offset_calc_x = Cpt(EpicsSignal, 'ComputePosOffsetX')
+    position_offset_calc_y = Cpt(EpicsSignal, 'ComputePosOffsetY')
+
+    position_scale_x = Cpt(EpicsSignal, 'PositionScaleX')
+    position_scale_Y = Cpt(EpicsSignal, 'PositionScaleY')
+
+    image = ADCpt(ImagePlugin, 'image1:')
+    current1 = ADCpt(StatsPluginV33, 'Current1:')
+    current2 = ADCpt(StatsPluginV33, 'Current2:')
+    current3 = ADCpt(StatsPluginV33, 'Current3:')
+    current4 = ADCpt(StatsPluginV33, 'Current4:')
+
+    sum_all = ADCpt(StatsPluginV33, 'SumAll:')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.stage_sigs.update([('acquire', 0),  # if acquiring, stop
+                                ('acquire_mode', 2)  # single mode
+                                ])
+        self._acquisition_signal = self.acquire
+
+        for i in range(1, 5):
+            current = getattr(self, 'current{}'.format(i))
+            current.mean_value.kind = Kind.hinted
