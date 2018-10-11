4af67096ca9c57aeb35bae98d51776e52a920bf2
Maksim Rakitin <maxim.rakitin@gmail.com>
2018-10-11 11:16:09 -0400

    ENH: Subclass QuadEM and update the current and sum_all fields

--- nslsii/ad33.py
+++ nslsii/ad33.py
@@ -8,19 +8,12 @@ all of these names may change in the future.
 """
 
 from ophyd import Device, Component as Cpt
-from ophyd.areadetector.base import (ADComponent as C, ad_group,
+from ophyd.areadetector.base import (ADComponent as ADCpt, ad_group,
                                      EpicsSignalWithRBV as SignalWithRBV)
 from ophyd.areadetector.plugins import PluginBase
 from ophyd.areadetector.trigger_mixins import TriggerBase, ADTriggerStatus
 from ophyd.device import DynamicDeviceComponent as DDC, Staged
-from ophyd.signal import (EpicsSignalRO, EpicsSignal, Signal)
-
-from ophyd import (Kind, kind_context)
-from ophyd.areadetector import (ADComponent as ADCpt, EpicsSignalWithRBV,
-                                ImagePlugin, StatsPlugin, DetectorBase,
-                                SingleTrigger, ADBase)
-from ophyd.status import DeviceStatus
-from ophyd import QuadEMPort, _current_fields
+from ophyd.signal import (EpicsSignalRO, EpicsSignal)
 
 import time as ttime
 
@@ -106,8 +99,8 @@ class StatsPluginV33(PluginBase):
         'profile_cursor')
     )
 
-    bgd_width = C(SignalWithRBV, 'BgdWidth')
-    centroid_threshold = C(SignalWithRBV, 'CentroidThreshold')
+    bgd_width = ADCpt(SignalWithRBV, 'BgdWidth')
+    centroid_threshold = ADCpt(SignalWithRBV, 'CentroidThreshold')
 
     centroid = DDC(ad_group(EpicsSignalRO,
                             (('x', 'CentroidX_RBV'),
@@ -115,10 +108,10 @@ class StatsPluginV33(PluginBase):
                    doc='The centroid XY',
                    default_read_attrs=('x', 'y'))
 
-    compute_centroid = C(SignalWithRBV, 'ComputeCentroid', string=True)
-    compute_histogram = C(SignalWithRBV, 'ComputeHistogram', string=True)
-    compute_profiles = C(SignalWithRBV, 'ComputeProfiles', string=True)
-    compute_statistics = C(SignalWithRBV, 'ComputeStatistics', string=True)
+    compute_centroid = ADCpt(SignalWithRBV, 'ComputeCentroid', string=True)
+    compute_histogram = ADCpt(SignalWithRBV, 'ComputeHistogram', string=True)
+    compute_profiles = ADCpt(SignalWithRBV, 'ComputeProfiles', string=True)
+    compute_statistics = ADCpt(SignalWithRBV, 'ComputeStatistics', string=True)
 
     cursor = DDC(ad_group(SignalWithRBV,
                           (('x', 'CursorX'),
@@ -126,11 +119,11 @@ class StatsPluginV33(PluginBase):
                  doc='The cursor XY',
                  default_read_attrs=('x', 'y'))
 
-    hist_entropy = C(EpicsSignalRO, 'HistEntropy_RBV')
-    hist_max = C(SignalWithRBV, 'HistMax')
-    hist_min = C(SignalWithRBV, 'HistMin')
-    hist_size = C(SignalWithRBV, 'HistSize')
-    histogram = C(EpicsSignalRO, 'Histogram_RBV')
+    hist_entropy = ADCpt(EpicsSignalRO, 'HistEntropy_RBV')
+    hist_max = ADCpt(SignalWithRBV, 'HistMax')
+    hist_min = ADCpt(SignalWithRBV, 'HistMin')
+    hist_size = ADCpt(SignalWithRBV, 'HistSize')
+    histogram = ADCpt(EpicsSignalRO, 'Histogram_RBV')
 
     max_size = DDC(ad_group(EpicsSignal,
                             (('x', 'MaxSizeX'),
@@ -138,15 +131,15 @@ class StatsPluginV33(PluginBase):
                    doc='The maximum size in XY',
                    default_read_attrs=('x', 'y'))
 
-    max_value = C(EpicsSignalRO, 'MaxValue_RBV')
+    max_value = ADCpt(EpicsSignalRO, 'MaxValue_RBV')
     max_xy = DDC(ad_group(EpicsSignalRO,
                           (('x', 'MaxX_RBV'),
                            ('y', 'MaxY_RBV'))),
                  doc='Maximum in XY',
                  default_read_attrs=('x', 'y'))
 
-    mean_value = C(EpicsSignalRO, 'MeanValue_RBV')
-    min_value = C(EpicsSignalRO, 'MinValue_RBV')
+    mean_value = ADCpt(EpicsSignalRO, 'MeanValue_RBV')
+    min_value = ADCpt(EpicsSignalRO, 'MinValue_RBV')
 
     min_xy = DDC(ad_group(EpicsSignalRO,
                           (('x', 'MinX_RBV'),
@@ -154,7 +147,7 @@ class StatsPluginV33(PluginBase):
                  doc='Minimum in XY',
                  default_read_attrs=('x', 'y'))
 
-    net = C(EpicsSignalRO, 'Net_RBV')
+    net = ADCpt(EpicsSignalRO, 'Net_RBV')
     profile_average = DDC(ad_group(EpicsSignalRO,
                                    (('x', 'ProfileAverageX_RBV'),
                                     ('y', 'ProfileAverageY_RBV'))),
@@ -185,13 +178,13 @@ class StatsPluginV33(PluginBase):
                             doc='Profile threshold in XY',
                             default_read_attrs=('x', 'y'))
 
-    set_xhopr = C(EpicsSignal, 'SetXHOPR')
-    set_yhopr = C(EpicsSignal, 'SetYHOPR')
-    sigma_xy = C(EpicsSignalRO, 'SigmaXY_RBV')
-    sigma_x = C(EpicsSignalRO, 'SigmaX_RBV')
-    sigma_y = C(EpicsSignalRO, 'SigmaY_RBV')
-    sigma = C(EpicsSignalRO, 'Sigma_RBV')
-    ts_acquiring = C(EpicsSignal, 'TS:TSAcquiring')
+    set_xhopr = ADCpt(EpicsSignal, 'SetXHOPR')
+    set_yhopr = ADCpt(EpicsSignal, 'SetYHOPR')
+    sigma_xy = ADCpt(EpicsSignalRO, 'SigmaXY_RBV')
+    sigma_x = ADCpt(EpicsSignalRO, 'SigmaX_RBV')
+    sigma_y = ADCpt(EpicsSignalRO, 'SigmaY_RBV')
+    sigma = ADCpt(EpicsSignalRO, 'Sigma_RBV')
+    ts_acquiring = ADCpt(EpicsSignal, 'TS:TSAcquiring')
 
     ts_centroid = DDC(ad_group(EpicsSignal,
                                (('x', 'TS:TSCentroidX'),
@@ -199,9 +192,9 @@ class StatsPluginV33(PluginBase):
                       doc='Time series centroid in XY',
                       default_read_attrs=('x', 'y'))
 
-    # ts_control = C(EpicsSignal, 'TS:TSControl', string=True)
-    ts_current_point = C(EpicsSignal, 'TS:TSCurrentPoint')
-    ts_max_value = C(EpicsSignal, 'TS:TSMaxValue')
+    # ts_control = ADCpt(EpicsSignal, 'TS:TSControl', string=True)
+    ts_current_point = ADCpt(EpicsSignal, 'TS:TSCurrentPoint')
+    ts_max_value = ADCpt(EpicsSignal, 'TS:TSMaxValue')
 
     ts_max = DDC(ad_group(EpicsSignal,
                           (('x', 'TS:TSMaxX'),
@@ -209,8 +202,8 @@ class StatsPluginV33(PluginBase):
                  doc='Time series maximum in XY',
                  default_read_attrs=('x', 'y'))
 
-    ts_mean_value = C(EpicsSignal, 'TS:TSMeanValue')
-    ts_min_value = C(EpicsSignal, 'TS:TSMinValue')
+    ts_mean_value = ADCpt(EpicsSignal, 'TS:TSMeanValue')
+    ts_min_value = ADCpt(EpicsSignal, 'TS:TSMinValue')
 
     ts_min = DDC(ad_group(EpicsSignal,
                           (('x', 'TS:TSMinX'),
@@ -218,100 +211,20 @@ class StatsPluginV33(PluginBase):
                  doc='Time series minimum in XY',
                  default_read_attrs=('x', 'y'))
 
-    ts_net = C(EpicsSignal, 'TS:TSNet')
-    ts_num_points = C(EpicsSignal, 'TS:TSNumPoints')
-    ts_read = C(EpicsSignal, 'TS:TSRead')
-    ts_sigma = C(EpicsSignal, 'TS:TSSigma')
-    ts_sigma_x = C(EpicsSignal, 'TS:TSSigmaX')
-    ts_sigma_xy = C(EpicsSignal, 'TS:TSSigmaXY')
-    ts_sigma_y = C(EpicsSignal, 'TS:TSSigmaY')
-    ts_total = C(EpicsSignal, 'TS:TSTotal')
-    total = C(EpicsSignalRO, 'Total_RBV')
+    ts_net = ADCpt(EpicsSignal, 'TS:TSNet')
+    ts_num_points = ADCpt(EpicsSignal, 'TS:TSNumPoints')
+    ts_read = ADCpt(EpicsSignal, 'TS:TSRead')
+    ts_sigma = ADCpt(EpicsSignal, 'TS:TSSigma')
+    ts_sigma_x = ADCpt(EpicsSignal, 'TS:TSSigmaX')
+    ts_sigma_xy = ADCpt(EpicsSignal, 'TS:TSSigmaXY')
+    ts_sigma_y = ADCpt(EpicsSignal, 'TS:TSSigmaY')
+    ts_total = ADCpt(EpicsSignal, 'TS:TSTotal')
+    total = ADCpt(EpicsSignalRO, 'Total_RBV')
 
 
-class QuadEMV33(SingleTrigger, DetectorBase):
-    # These settings intentionally shadow the settings inherited from
-    # DetectorBase.
-    _default_read_attrs = None
-    _default_configuration_attrs = None
-
-    _status_type = DeviceStatus  # overrriding the default in SingleTrigger
-
-    # This is needed because ophyd verifies that it can see all
-    # of the nodes in the asyn pipeline, however these IOCs do not
-    # expose their port name via a PV, but nevertheless server as the
-    # root node for the plugins.
-    # Leaving this port_name here for compatibility
-    integration_time = Cpt(EpicsSignalWithRBV, 'IntegrationTime',
-                           kind='config')
-    averaging_time = Cpt(EpicsSignalWithRBV, 'AveragingTime', kind='config')
-    with kind_context('omitted') as OCpt:
-        conf = Cpt(QuadEMPort, port_name='NSLS_EM')
-        model = OCpt(EpicsSignalRO, 'Model')
-        firmware = OCpt(EpicsSignalRO, 'Firmware')
-
-        acquire_mode = OCpt(EpicsSignalWithRBV, 'AcquireMode')
-        acquire = OCpt(EpicsSignal, 'Acquire')
-
-        read_format = OCpt(EpicsSignalWithRBV, 'ReadFormat')
-        em_range = OCpt(EpicsSignalWithRBV, 'Range')
-        ping_pong = OCpt(EpicsSignalWithRBV, 'PingPong')
-
-        num_channels = OCpt(EpicsSignalWithRBV, 'NumChannels')
-        geometry = OCpt(EpicsSignalWithRBV, 'Geometry')
-        resolution = OCpt(EpicsSignalWithRBV, 'Resolution')
-
-        bias_state = OCpt(EpicsSignalWithRBV, 'BiasState')
-        bias_interlock = OCpt(EpicsSignalWithRBV, 'BiasInterlock')
-        bias_voltage = OCpt(EpicsSignalWithRBV, 'BiasVoltage')
-        hvs_readback = OCpt(EpicsSignalRO, 'HVSReadback')
-        hvv_readback = OCpt(EpicsSignalRO, 'HVVReadback')
-        hvi_readback = OCpt(EpicsSignalRO, 'HVIReadback')
-
-        values_per_read = OCpt(EpicsSignalWithRBV, 'ValuesPerRead')
-        sample_time = OCpt(EpicsSignalRO, 'SampleTime_RBV')  # yay consistency
-        num_average = OCpt(EpicsSignalRO, 'NumAverage_RBV')
-        num_averaged = OCpt(EpicsSignalRO, 'NumAveraged_RBV')
-        num_acquire = OCpt(EpicsSignalWithRBV, 'NumAcquire')
-        num_acquired = OCpt(EpicsSignalRO, 'NumAcquired')
-        read_data = OCpt(EpicsSignalRO, 'ReadData')
-        ring_overflows = OCpt(EpicsSignalRO, 'RingOverflows')
-        trigger_mode = OCpt(EpicsSignal, 'TriggerMode')
-        reset = OCpt(EpicsSignal, 'Reset')
-
-    current_names = DDCpt(_current_fields('ch', 'CurrentName', range(1, 5),
-                                          string=True))
-    current_offsets = DDCpt(_current_fields('ch', 'CurrentOffset',
-                                            range(1, 5)))
-    current_offset_calcs = DDCpt(_current_fields('ch', 'ComputeCurrentOffset',
-                                                 range(1, 5)))
-    current_scales = DDCpt(_current_fields('ch', 'CurrentScale', range(1, 5)))
-
-    position_offset_x = Cpt(EpicsSignal, 'PositionOffsetX')
-    position_offset_y = Cpt(EpicsSignal, 'PositionOffsetY')
-
-    position_offset_calc_x = Cpt(EpicsSignal, 'ComputePosOffsetX')
-    position_offset_calc_y = Cpt(EpicsSignal, 'ComputePosOffsetY')
-
-    position_scale_x = Cpt(EpicsSignal, 'PositionScaleX')
-    position_scale_Y = Cpt(EpicsSignal, 'PositionScaleY')
-
-    image = ADCpt(ImagePlugin, 'image1:')
+class QuadEMV33(QuadEM):
     current1 = ADCpt(StatsPluginV33, 'Current1:')
     current2 = ADCpt(StatsPluginV33, 'Current2:')
     current3 = ADCpt(StatsPluginV33, 'Current3:')
     current4 = ADCpt(StatsPluginV33, 'Current4:')
-
     sum_all = ADCpt(StatsPluginV33, 'SumAll:')
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.stage_sigs.update([('acquire', 0),  # if acquiring, stop
-                                ('acquire_mode', 2)  # single mode
-                                ])
-        self._acquisition_signal = self.acquire
-
-        for i in range(1, 5):
-            current = getattr(self, 'current{}'.format(i))
-            current.mean_value.kind = Kind.hinted
