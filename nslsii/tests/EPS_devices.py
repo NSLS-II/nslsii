efc27c7367790ad2e6f8d546107224ac6d27278d
Andrew Walter <awalter@bnl.gov>
2019-02-22 15:28:11 -0500

    ENH: fixing a flake8 issue

--- nslsii/tests/EPS_devices.py
+++ nslsii/tests/EPS_devices.py
@@ -1,10 +1,10 @@
 
 from nslsii.EPS_devices.valves_and_shutters import EPSTwoStateDevice
-import pytest
+
 
 def test_EPSTwoStateDevice():
     '''A smoke test of the EPSTwoStateDevice class
     '''
 
-    shutter=EPSTwoStateDevice(prefix='test', name='shutter')
-    assert(hasattr(shutter,'read'))
+    shutter = EPSTwoStateDevice(prefix='test', name='shutter')
+    assert(hasattr(shutter, 'read'))
