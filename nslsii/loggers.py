1c7749420bfc45b77a5ab5fef134f8b677387215
ke-zhang-rd <kz2249@columbia.edu>
2019-03-26 15:20:36 -0400

    MNT: Update dict for es

--- nslsii/loggers.py
+++ nslsii/loggers.py
@@ -1,6 +1,6 @@
 import logging
 import requests
-
+import json
 
 class ElasticHandler(logging.Handler):
     '''
@@ -27,13 +27,16 @@ class ElasticHandler(logging.Handler):
         # Extract useful info from the record and put them into a dict.
         try:
             record_dict = {'name': record.name,
-                           'levelno': record.levelno,
+                           'levelname': record.levelname,
                            'pathname': record.pathname,
                            'lineno': record.lineno,
-                           'msg': record.msg,
-                           'args': record.args,
-                           'exc_info': record.exc_info,
-                           'sinfo': record.sinfo}
+                           'msg': self.format(record),
+                           'args': list(map(str, record.args)),
+                           'threadName': record.threadName}
+
+            if hasattr(record, 'pv'): record_dict['pv'] = record.pv
+            if hasattr(record, 'address'): record_dict['address'] = record.address
+            if hasattr(record, 'role'): record_dict['role'] = record.role
             response = requests.post(self.url, json=record_dict)
             response.raise_for_status()
         except Exception:
@@ -41,13 +44,14 @@ class ElasticHandler(logging.Handler):
 
 def configure_elastic(beamline):
     url = 'http://elasticsearch.cs.nsls2.local/' + beamline + '.blueksy'
+    local_url = 'http://localhost:9200/test1/_doc'
 
     bluesky_logger = logging.getLogger('bluesky')
     bluesky_elastic_hdr = ElasticHandler(url, level = logging.DEBUG)
     bluesky_logger.addHandler(elastic_hdr)
 
     carproto_logger = logging.getLogger('carproto')
-    carproto_elastic_hdr = ElasticHandler(url, level = logging.INFO)
+    carproto_elastic_hdr = ElasticHandler(local_url, level = logging.DEBUG)
     carproto_logger.addHandler(elastic_hdr)
 
     ophyd_logger = logging.getLogger('ophyd')
