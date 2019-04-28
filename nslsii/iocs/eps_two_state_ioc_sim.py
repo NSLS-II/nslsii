cde291a2620c4466297138268bd8d5835f88f60b
nmalitsky <nmalitsky@gmail.com>
2019-04-28 17:57:01 -0400

    added iocs/eps_two_state_ioc_sim.py

---
+++ nslsii/iocs/eps_two_state_ioc_sim.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+from caproto.server import pvproperty, PVGroup
+from caproto.server import template_arg_parser, run
+from caproto import ChannelType
+
+
+class EPSTwoStateIOC(PVGroup):
+    """
+    Simulates multiple-attempt issue for two-state device.
+    Parameters
+    ----------
+    retries : int, optional
+        Number of attempts required for changing state,
+        default is 2
+    enbl_sts_val: str, optional
+        Enumerated string that enables the state change,
+        default is True
+    hw_error_val: str, optional
+        Enumerated string that activates the hardware error,
+        default is False
+    sts_error_val: str, optional
+        Enumerated string that activates the Pos-Sts error,
+        default is False
+    """
+
+    def __init__(self, retries=2, enbl_sts_val='True',
+                 hw_error_val='False', sts_error_val='False',
+                 **kwargs):
+
+        super().__init__(**kwargs)
+
+        self._max_retries = retries
+        self._num_retries = 0
+
+        self._enbl_sts_val = enbl_sts_val
+
+        self._hw_error_val = hw_error_val
+        self._sts_error_val = sts_error_val
+
+    # Pos-Sts two-state PV
+
+    _pos_states = ['Open', 'Closed']  # two position states
+    _pos_sts_val = _pos_states[0]
+
+    pos_sts = pvproperty(value=_pos_sts_val,
+                         enum_strings=_pos_states,
+                         dtype=ChannelType.ENUM,
+                         read_only=True,
+                         name='Pos-Sts')
+
+    # Opn-Cmd and Cls-Cmd PVs used by client for changing state
+
+    _cmd_states = ['None', 'Done']  # two command states
+
+    state1_cmd = pvproperty(value=_cmd_states[0],
+                            enum_strings=_cmd_states,
+                            dtype=ChannelType.ENUM,
+                            name='Cmd:Opn-Cmd')
+    state2_cmd = pvproperty(value=_cmd_states[0],
+                            enum_strings=_cmd_states,
+                            dtype=ChannelType.ENUM,
+                            name='Cmd:Cls-Cmd')
+
+    _fail_states = ['False', 'True']
+
+    fail_to_state1 = pvproperty(value=_fail_states[0],
+                                enum_strings=_fail_states,
+                                dtype=ChannelType.ENUM,
+                                read_only=True,
+                                name='Sts:FailOpn-Sts')
+    fail_to_state2 = pvproperty(value=_fail_states[0],
+                                enum_strings=_fail_states,
+                                dtype=ChannelType.ENUM,
+                                read_only=True,
+                                name='Sts:FailCls-Sts')
+
+    # Enbl-Sts PV that enables/disables the state change
+
+    _enbl_states = ['True', 'False']
+
+    enbl_sts = pvproperty(value='',
+                          enum_strings=_enbl_states,
+                          dtype=ChannelType.ENUM,
+                          read_only=True,
+                          name='Enbl-Sts')
+
+    # PV Startup/Putter Methods
+
+    @enbl_sts.startup
+    async def enbl_sts(self, instance, async_lib):
+        print('in enbl_sts', 'enbl_sts_val:', self._enbl_sts_val)
+        await instance.write(value=self._enbl_sts_val)
+
+    @state1_cmd.putter
+    async def state1_cmd(self, instance, value):
+        rv = await self._state_cmd_put(instance, value,
+                                       self._pos_states[0],
+                                       self.fail_to_state1)
+        print('in state1_cmd', 'value:', value, 'rv:', rv)
+        return rv
+
+    @state2_cmd.putter
+    async def state2_cmd(self, instance, value):
+        rv = await self._state_cmd_put(instance, value,
+                                       self._pos_states[1],
+                                       self.fail_to_state2)
+        print('in state2_cmd', 'value:', value, 'rv:', rv)
+        return rv
+
+    # Internal Methods
+
+    async def _state_cmd_put(self, instance, value, state_val, fail_to_state):
+        print('1. state_cmd_put:', 'value:', value, 'state_val:', state_val)
+        if(value == self._cmd_states[0]):  # if None -> do nothing
+            return self._cmd_states[0]
+        if(self._pos_sts_val == state_val):  # if in state -> do nothing
+            return self._cmd_states[0]
+        if(self._enbl_sts_val == 'False'):  # if changes not enabled -> fail
+            await fail_to_state.write(value='True')
+            return self._cmd_states[0]
+        self._num_retries += 1
+        if(self._num_retries < self._max_retries):
+            return self._cmd_states[0]
+        else:
+            self._num_retries = 0
+        if(self._hw_error_val == 'True'):  # if hw error -> fail
+            await fail_to_state.write(value='True')
+            return self._cmd_states[1]
+        if(self._sts_error_val == 'True'):  # if sts error -> don't change sts
+            return self._cmd_states[1]
+        await self.pos_sts.write(value=state_val)
+        self._pos_sts_val = state_val
+        print('pos_sts.write:', state_val)
+        return self._cmd_states[0]
+
+
+if __name__ == '__main__':
+
+    parser, split_args = template_arg_parser(
+        default_prefix='eps2state:',
+        desc='EPS Two State IOC.')
+
+    retries_help = 'Number of attempts required for changing state'
+    enable_help = 'State change is enabled'
+    hwerror_help = 'HW error is activated'
+    stserror_help = 'Pos-Sts error is activated'
+
+    parser.add_argument('--retries', help=retries_help,
+                        required=False, default=2, type=int)
+    parser.add_argument('--enable', help=enable_help,
+                        required=False, default='True', type=str)
+    parser.add_argument('--hwerror', help=hwerror_help,
+                        required=False, default='False', type=str)
+    parser.add_argument('--stserror', help=stserror_help,
+                        required=False, default='False', type=str)
+
+    args = parser.parse_args()
+    ioc_options, run_options = split_args(args)
+
+    print(ioc_options, args.retries, args.enable, args.hwerror, args.stserror)
+
+    ioc = EPSTwoStateIOC(retries=args.retries,
+                         enbl_sts_val=args.enable,
+                         hw_error_val=args.hwerror,
+                         sts_error_val=args.stserror,
+                         **ioc_options)
+
+    run(ioc.pvdb, **run_options)
---
+++ nslsii/iocs/tests/test_epstwostate_ioc.py
@@ -0,0 +1,97 @@
+import subprocess
+import os
+import sys
+
+from ophyd import Device, EpicsSignal, EpicsSignalRO
+from ophyd.device import (Component as Cpt,
+                          FormattedComponent as FmtCpt)
+
+
+class EPSTwoStateDevice(Device):
+
+    def __init__(self, *, prefix, name, **kwargs):
+        super().__init__(prefix=prefix, name=name, **kwargs)
+
+    status = Cpt(EpicsSignalRO, 'Pos-Sts', string=True)
+
+    opn_cmd = FmtCpt(EpicsSignal,
+                     '{self.prefix}Cmd:Opn-Cmd',
+                     string=True, kind='omitted')
+    cls_cmd = FmtCpt(EpicsSignal,
+                     '{self.prefix}Cmd:Cls-Cmd',
+                     string=True, kind='omitted')
+
+
+def test_epstwostate_ioc():
+
+    stdout = subprocess.PIPE
+    stdin = None
+
+    # Start up EPSTwoStateIOC requiring two attempts
+    ioc_process = subprocess.Popen([sys.executable, '-m',
+                                    'caproto.tests.example_runner',
+                                    'nslsii.iocs.eps_two_state_ioc_sim'],
+                                   stdout=stdout, stdin=stdin,
+                                   env=os.environ)
+
+    print(f'nslsii.iocs.epc_two_state_ioc_sim is now running')
+
+    # Wrap the rest in a try-except to ensure the ioc is killed before exiting
+    try:
+
+        eps = EPSTwoStateDevice(prefix='eps2state:', name='eps')
+
+        # 1. check the ioc-device connection and initial values
+        sts_val = eps.status.get()
+        assert sts_val == 'Open'
+
+        opn_cmd_val = eps.opn_cmd.get()
+        assert opn_cmd_val == 'None'
+
+        cls_cmd_val = eps.cls_cmd.get()
+        assert cls_cmd_val == 'None'
+
+        # 2. try to close with one attempt
+
+        eps.cls_cmd.put(1)
+
+        cls_cmd_val = eps.cls_cmd.get()
+        assert cls_cmd_val == 'None'
+
+        sts_val = eps.status.get()
+        assert sts_val == 'Open'
+
+        # 3. try to close with second attempt
+
+        eps.cls_cmd.put(1)
+
+        cls_cmd_val = eps.cls_cmd.get()
+        assert cls_cmd_val == 'None'
+
+        sts_val = eps.status.get()
+        assert sts_val == 'Closed'
+
+        # 4. try to open with one attempt
+
+        eps.opn_cmd.put(1)
+
+        opn_cmd_val = eps.opn_cmd.get()
+        assert opn_cmd_val == 'None'
+
+        sts_val = eps.status.get()
+        assert sts_val == 'Closed'
+
+        # 5. try to open with second attempt
+
+        eps.opn_cmd.put(1)
+
+        opn_cmd_val = eps.opn_cmd.get()
+        assert opn_cmd_val == 'None'
+
+        sts_val = eps.status.get()
+        assert sts_val == 'Open'
+
+    finally:
+        # Ensure that for any exception the ioc sub-process is terminated
+        # before raising.
+        ioc_process.terminate()
