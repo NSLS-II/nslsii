32d4d4cb86ca3cbb2cf2c2e087ea93bdf34fa915
Dan Allan <dallan@bnl.gov>
2020-03-13 13:34:01 -0400

    Fix syntax. Add docstring.

--- nslsii/transforms.py
+++ nslsii/transforms.py
@@ -3,7 +3,13 @@
 import copy
 
 
-def fix_csx_scaler_shape(d)
+def fix_csx_scaler_shape(d):
+    """
+    Transform an Event Descriptor.
+
+    Look for sources that match the pattern of a Scaler.
+    Ensure that their shape is [].
+    """
     d = copy.deepcopy(d)
     for k, v in list(d['data_keys'].items()):
         if v['source'].startswith('PV:XF:23ID1-ES{Sclr:1}Wfrm'):
