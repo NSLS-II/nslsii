b0e8e882be16acaa7dea49f3a82d8aebbb7e0a7e
Joshua Lynch <jlynch@bnl.gov>
2020-06-11 12:14:55 -0400

    add CMB02 and CMB03 to the list of kafka bootstrap servers

--- nslsii/__init__.py
+++ nslsii/__init__.py
@@ -208,7 +208,7 @@ def configure_base(
         subscribe_kafka_publisher(
             RE,
             beamline_name=broker_name,
-            bootstrap_servers="10.0.137.8:9092"
+            bootstrap_servers="10.0.137.8:9092,10.0.137.21:9092,10.0.137.22:9092"
         )
 
     # always configure %xmode minimal
