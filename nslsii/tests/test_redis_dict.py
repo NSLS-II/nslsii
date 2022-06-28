fdcc9b46cd82949d3f9a3a80154130280c9f8fe4
jklynch <jlynch@bnl.gov>
2022-06-28 10:13:16 -0400

    test numpy arrays

--- nslsii/tests/test_redis_dict.py
+++ nslsii/tests/test_redis_dict.py
@@ -3,6 +3,7 @@ import time
 
 import pytest
 
+import numpy as np
 import redis
 import redis.exceptions
 
@@ -44,12 +45,7 @@ def test_instantiate_with_server(redis_dict_factory):
     """
     Instantiate a RunEngineRedisDict and expect success.
     """
-    try:
-        redis_dict = redis_dict_factory(
-            re_md_channel_name="test_instantiate_with_server"
-        )
-    finally:
-        redis_dict._update_on_message_thread.stop()
+    redis_dict = redis_dict_factory(re_md_channel_name="test_instantiate_with_server")
 
 
 def test_instantiate_no_server():
@@ -59,8 +55,6 @@ def test_instantiate_no_server():
     """
     with pytest.raises(redis.exceptions.ConnectionError):
         # there is no redis server on port 9999
-        # note: __init__ fails before _update_on_message_thread is started
-        # so there is no need to explicitly stop that thread in this test
         RunEngineRedisDict(host="localhost", port=9999)
 
 
@@ -68,38 +62,39 @@ def test_local_int_value(redis_dict_factory):
     """
     Test that an integer is stored and retrieved.
     """
-    try:
-        redis_dict = redis_dict_factory(re_md_channel_name="test_local_int_value")
-        redis_dict["local_int"] = -1
-        assert redis_dict["local_int"] == -1
-    finally:
-        redis_dict._update_on_message_thread.stop()
+    redis_dict = redis_dict_factory(re_md_channel_name="test_local_int_value")
+    redis_dict["local_int"] = -1
+    assert redis_dict["local_int"] == -1
 
 
 def test_local_float_value(redis_dict_factory):
     """
     Test that a float is stored and retrieved.
     """
-    try:
-        redis_dict = redis_dict_factory(re_md_channel_name="test_local_float_value")
-        import math
+    redis_dict = redis_dict_factory(re_md_channel_name="test_local_float_value")
+    import math
 
-        redis_dict["local_float"] = math.pi
-        assert redis_dict["local_float"] == math.pi
-    finally:
-        redis_dict._update_on_message_thread.stop()
+    redis_dict["local_float"] = math.pi
+    assert redis_dict["local_float"] == math.pi
+
+
+def test_local_ndarray_value(redis_dict_factory):
+    """
+    Test that a numpy NDArray is stored and retrieved.
+    """
+    redis_dict = redis_dict_factory(re_md_channel_name="test_local_float_value")
+
+    redis_dict["local_array"] = np.ones((10, 10))
+    assert np.array_equal(redis_dict["local_array"], np.ones((10, 10)))
 
 
 def test_del_global_key(redis_dict_factory):
     """
     Test that attempting to delete a "global" key raised KeyError.
     """
-    try:
-        redis_dict = redis_dict_factory(re_md_channel_name="test_del_global_key")
-        with pytest.raises(KeyError):
-            del redis_dict[redis_dict._global_keys[0]]
-    finally:
-        redis_dict._update_on_message_thread.stop()
+    redis_dict = redis_dict_factory(re_md_channel_name="test_del_global_key")
+    with pytest.raises(KeyError):
+        del redis_dict[redis_dict._global_keys[0]]
 
 
 def test_del_local_key(redis_dict_factory):
@@ -107,134 +102,126 @@ def test_del_local_key(redis_dict_factory):
     Test that a "local" key can be deleted and that
     a message is published.
     """
-    try:
-        redis_dict = redis_dict_factory(re_md_channel_name="test_del_local_key")
-        redis_subscriber = _build_redis_subscriber(redis_dict)
+    redis_dict = redis_dict_factory(re_md_channel_name="test_del_local_key")
+    redis_subscriber = _build_redis_subscriber(redis_dict)
 
-        redis_dict["local_key"] = "local_value"
-        assert redis_dict["local_key"] == "local_value"
+    redis_dict["local_key"] = "local_value"
+    assert redis_dict["local_key"] == "local_value"
 
-        _local_md = redis_dict._get_local_metadata_from_server()
-        assert _local_md["local_key"] == "local_value"
+    _local_md = redis_dict._get_local_metadata_from_server()
+    assert _local_md["local_key"] == "local_value"
 
-        del redis_dict["local_key"]
-        _local_md = redis_dict._get_local_metadata_from_server()
+    del redis_dict["local_key"]
+    _local_md = redis_dict._get_local_metadata_from_server()
 
-        assert "local_key" not in _local_md
-        assert "local_key" not in redis_dict
+    assert "local_key" not in _local_md
+    assert "local_key" not in redis_dict
 
-        messages = _get_waiting_messages(redis_subscriber)
-        assert len(messages) == 2
+    messages = _get_waiting_messages(redis_subscriber)
+    assert len(messages) == 2
 
-        for message in messages:
-            updated_key, publisher_uuid = RunEngineRedisDict._parse_message_data(message)
-            assert updated_key == "local_key"
-            assert publisher_uuid == redis_dict._uuid
-
-    finally:
-        redis_dict._update_on_message_thread.stop()
+    for message in messages:
+        updated_key, publisher_uuid = RunEngineRedisDict._parse_message_data(message)
+        assert updated_key == "local_key"
+        assert publisher_uuid == redis_dict._uuid
 
 
 def test_items(redis_dict_factory):
     """
     Test .items() starting from an empty Redis database.
     """
-    try:
-        redis_dict = redis_dict_factory(re_md_channel_name="test_items")
+    redis_dict = redis_dict_factory(re_md_channel_name="test_items")
 
-        # expect to find the global keys with value None
-        expected_global_items = {gk: None for gk in redis_dict._global_keys}
-        actual_global_items = {gk: gv for gk, gv in redis_dict.items()}
-        assert actual_global_items == expected_global_items
+    # expect to find the global keys with value None
+    expected_global_items = {gk: None for gk in redis_dict._global_keys}
+    actual_global_items = {gk: gv for gk, gv in redis_dict.items()}
+    assert actual_global_items == expected_global_items
 
-        # set a value for each global key
-        global_md_updates = {gk: gk for gk in redis_dict._global_keys}
-        redis_dict.update(global_md_updates)
-        actual_global_items = {gk: gv for gk, gv in redis_dict.items()}
-        # _local_md should still be empty
-        # since only global metadata was updated
-        # this is not the default behavior of ChainMap!
-        assert len(redis_dict._local_md) == 0
-        assert actual_global_items == global_md_updates
+    # set a value for each global key
+    global_md_updates = {gk: gk for gk in redis_dict._global_keys}
+    redis_dict.update(global_md_updates)
+    actual_global_items = {gk: gv for gk, gv in redis_dict.items()}
+    # _local_md should still be empty
+    # since only global metadata was updated
+    # this is not the default behavior of ChainMap!
+    assert len(redis_dict._local_md) == 0
+    assert actual_global_items == global_md_updates
 
-        # set some local metadata
-        local_md_updates = {"one": 1, "two": "2"}
-        redis_dict.update(local_md_updates)
-        assert redis_dict._local_md == local_md_updates
-        expected_items = dict()
-        expected_items.update(global_md_updates)
-        expected_items.update(local_md_updates)
-        actual_items = {k: v for k, v in redis_dict.items()}
-        assert actual_items == expected_items
-    finally:
-        redis_dict._update_on_message_thread.stop()
+    # set some local metadata
+    local_md_updates = {"one": 1, "two": "2"}
+    redis_dict.update(local_md_updates)
+    assert redis_dict._local_md == local_md_updates
+    expected_items = dict()
+    expected_items.update(global_md_updates)
+    expected_items.update(local_md_updates)
+    actual_items = {k: v for k, v in redis_dict.items()}
+    assert actual_items == expected_items
 
 
 def test_one_message(redis_dict_factory):
-    try:
-        redis_dict = redis_dict_factory(re_md_channel_name="test_one_message")
-        print(redis_dict._local_md)
-        assert len(redis_dict._local_md) == 0
+    redis_dict = redis_dict_factory(re_md_channel_name="test_one_message")
+    print(redis_dict._local_md)
+    assert len(redis_dict._local_md) == 0
 
-        redis_subscriber = _build_redis_subscriber(redis_dict)
+    redis_subscriber = _build_redis_subscriber(redis_dict)
 
-        # expect no messages
-        messages = _get_waiting_messages(redis_subscriber)
-        assert len(messages) == 0
+    # expect no messages
+    messages = _get_waiting_messages(redis_subscriber)
+    assert len(messages) == 0
 
-        redis_dict["generate_one_message"] = 1
-        # expect one message
-        messages = _get_waiting_messages(redis_subscriber)
-        print(f"messages:\n {pformat(messages)}")
-        assert len(messages) == 1
-        updated_key, publisher_uuid = redis_dict._parse_message_data(messages[0])
-        assert updated_key == "generate_one_message"
-        assert publisher_uuid == redis_dict._uuid
-    finally:
-        redis_dict._update_on_message_thread.stop()
+    redis_dict["generate_one_message"] = 1
+    # expect one message
+    messages = _get_waiting_messages(redis_subscriber)
+    print(f"messages:\n {pformat(messages)}")
+    assert len(messages) == 1
+    updated_key, publisher_uuid = redis_dict._parse_message_data(messages[0])
+    assert updated_key == "generate_one_message"
+    assert publisher_uuid == redis_dict._uuid
 
 
 def test_two_messages(redis_dict_factory):
-    try:
-        redis_dict = redis_dict_factory(re_md_channel_name="test_two_messages")
-        print(redis_dict._local_md)
-        assert len(redis_dict._local_md) == 0
+    redis_dict = redis_dict_factory(re_md_channel_name="test_two_messages")
+    print(redis_dict._local_md)
+    assert len(redis_dict._local_md) == 0
 
-        redis_subscriber = _build_redis_subscriber(redis_dict)
+    redis_subscriber = _build_redis_subscriber(redis_dict)
 
-        redis_dict["generate_one_message"] = 1
-        redis_dict["generate_another_message"] = 2
+    redis_dict["generate_one_message"] = 1
+    redis_dict["generate_another_message"] = 2
 
-        messages = _get_waiting_messages(redis_subscriber)
-        assert len(messages) == 2
-        for message, expected_updated_key in zip(messages, redis_dict._local_md.keys()):
-            actual_updated_key, publisher_uuid = redis_dict._parse_message_data(message)
-            assert actual_updated_key == expected_updated_key
-            assert publisher_uuid == redis_dict._uuid
-    finally:
-        redis_dict._update_on_message_thread.stop()
+    messages = _get_waiting_messages(redis_subscriber)
+    assert len(messages) == 2
+    for message, expected_updated_key in zip(messages, redis_dict._local_md.keys()):
+        actual_updated_key, publisher_uuid = redis_dict._parse_message_data(message)
+        assert actual_updated_key == expected_updated_key
+        assert publisher_uuid == redis_dict._uuid
 
 
 def test_synchronization(redis_dict_factory):
     """
     Test synchronization between separate RunEngineRedisDicts.
     """
-    try:
-        redis_dict_1 = redis_dict_factory(re_md_channel_name="test_synchronization")
-        redis_dict_2 = redis_dict_factory(re_md_channel_name="test_synchronization")
-        redis_dict_2_subscriber = _build_redis_subscriber(redis_dict_2)
+    redis_dict_1 = redis_dict_factory(re_md_channel_name="test_synchronization")
+    redis_dict_2 = redis_dict_factory(re_md_channel_name="test_synchronization")
+    redis_dict_2_subscriber = _build_redis_subscriber(redis_dict_2)
 
-        # make one change
-        redis_dict_1["one"] = "two"
-        time.sleep(1)
-        redis_dict_2_messages = _get_waiting_messages(redis_dict_2_subscriber)
-        assert len(redis_dict_2_messages) == 1
-        assert redis_dict_2["one"] == "two"
+    # make one change
+    redis_dict_1["string"] = "string"
+    redis_dict_1["int"] = 0
+    redis_dict_1["float"] = np.pi
+    redis_dict_1["array"] = np.ones((10, 10))
 
-        redis_dict_3 = redis_dict_factory(re_md_channel_name="test_synchronization")
-        assert redis_dict_3["one"] == "two"
+    time.sleep(1)
+    redis_dict_2_messages = _get_waiting_messages(redis_dict_2_subscriber)
+    assert len(redis_dict_2_messages) == 4
 
-    finally:
-        redis_dict_1._update_on_message_thread.stop()
-        redis_dict_2._update_on_message_thread.stop()
-        redis_dict_3._update_on_message_thread.stop()
+    assert redis_dict_2["string"] == "string"
+    assert redis_dict_2["int"] == 0
+    assert redis_dict_2["float"] == np.pi
+    assert np.array_equal(redis_dict_2["array"], np.ones((10, 10)))
+
+    redis_dict_3 = redis_dict_factory(re_md_channel_name="test_synchronization")
+    assert redis_dict_3["string"] == "string"
+    assert redis_dict_3["int"] == 0
+    assert redis_dict_3["float"] == np.pi
+    assert np.array_equal(redis_dict_3["array"], np.ones((10, 10)))
