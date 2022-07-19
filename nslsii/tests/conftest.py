190845feaed3ba39c1203dcd33fd3c2bef40835f
Joshua Lynch <jlynch@bnl.gov>
2022-07-19 12:14:39 -0400

    restrict fixture redis_dict_factory to redis on localhost:6379

--- nslsii/tests/conftest.py
+++ nslsii/tests/conftest.py
@@ -23,21 +23,31 @@ def redis_dict_factory():
     """
     Return a "fixture as a factory" that will build identical RunEngineRedisDicts.
     Before the factory is returned, the Redis server will be cleared.
+
+    The factory builds only RunEngineRedisDict instances for a Redis server running
+    on localhost:6379, db=0.
+
+    If "host", "port", or "db" are specified as kwargs to the factory function
+    an exception will be raised.
     """
-    redis_client = redis.Redis(host="localhost", port=6379, db=0)
+    redis_server_kwargs = {
+        "host": "localhost",
+        "port": 6379,
+        "db": 0,
+    }
+
+    redis_client = redis.Redis(**redis_server_kwargs)
     redis_client.flushdb()
 
-    def _factory(re_md_channel_name, **kwargs):
-        init_kwargs = {
-            "host": "localhost",
-            "port": 6379,
-            "db": 0,
-            "re_md_channel_name": re_md_channel_name
-        }
-        init_kwargs.update(kwargs)
+    def _factory(**kwargs):
+        disallowed_kwargs_preset = set(redis_server_kwargs.keys()).intersection(kwargs.keys())
+        if len(disallowed_kwargs_preset) > 0:
+            raise KeyError(
+                f"{disallowed_kwargs_preset} given, but 'host', 'port', and 'db' may not be specified"
+            )
+        else:
+            kwargs.update(redis_server_kwargs)
 
-        return RunEngineRedisDict(**init_kwargs)
+        return RunEngineRedisDict(**kwargs)
 
     return _factory
-
-
