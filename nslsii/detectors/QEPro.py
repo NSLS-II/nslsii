4c1377aa926ae7a60ac2d30c12f578cf662c672f
Jakub Wlodek <jwlodek@bnl.gov>
2022-08-08 16:37:40 -0400

    Updates to QEPro class, mostly workgin

--- nslsii/detectors/QEPro.py
+++ nslsii/detectors/QEPro.py
@@ -1,27 +1,40 @@
+from gc import collect
 import logging
 
 from ophyd import (Device, Component as Cpt, FormattedComponent as FC,
                    Signal)
 from ophyd import (EpicsSignal, EpicsSignalRO, DeviceStatus, DerivedSignal)
 from ophyd.areadetector import EpicsSignalWithRBV as SignalWithRBV
+from ophyd.status import SubscriptionStatus
 
-#class QEProTECAtSetPoint(DerivedSignal):
-#    def __init__(self, trigger, *, parent=None, **kwargs):
-#        trigger = getattr(parent, trigger)
-#        super().__init__(derived_from=trigger, parent=parent, **kwargs)
-#
-#    def inverse(self, value):
-#        if abs(value - curr_tec_temp.get()) < .5:
-#            return 1
-#        else:
-#            return 0
-#
-#    def forward(self, value):
-#        return value
+class QEProTEC(Device):
+
+    # Thermal electric cooler settings
+    tec = Cpt(SignalWithRBV, 'TEC')
+    tec_temp = Cpt(SignalWithRBV, 'TEC_TEMP')
+    curr_tec_temp = Cpt(EpicsSignalRO, 'CURR_TEC_TEMP_RBV')
+
+    def __init__(self, *args, tolerance=1, **kwargs):
+        self.tolerance = tolerance
+        super().__init__(*args, **kwargs)
+
+    def set(self, value):
+
+        def check_setpoint(value, old_value, **kwargs):
+            if abs(value - self.tec_temp.get()) < self.tolerance:
+                print(f'Reached setpoint {self.tec_temp.get()}.')
+                return True
+            return False
+
+        status = SubscriptionStatus(self.curr_tec_temp, run=False, callback=check_setpoint)
+        self.tec_temp.put(value)
+        self.tec.put(1)
+
+        return status
 
 
 class QEPro(Device):
-   
+
     # Device information
     serial = Cpt(EpicsSignal, 'SERIAL')
     model = Cpt(EpicsSignal, 'MODEL')
@@ -29,7 +42,7 @@ class QEPro(Device):
     # Device Status
     status = Cpt(EpicsSignal, 'STATUS')
     status_msg = Cpt(EpicsSignal, 'STATUS_MSG')
-    connected = Cpt(EpicsSignalRO, 'CONNECTED_RBV')
+    device_connected = Cpt(EpicsSignalRO, 'CONNECTED_RBV')
 
     # Utility signal that periodically checks device temps.
     __check_status = Cpt(EpicsSignal, 'CHECK_STATUS')
@@ -42,11 +55,9 @@ class QEPro(Device):
     electric_dark_correction = Cpt(SignalWithRBV, 'EDC')
     non_linearity_correction = Cpt(SignalWithRBV, 'NLC')
     shutter = Cpt(SignalWithRBV, 'SHUTTER')
-   
-    # Thermal electric cooler settings
-    tec = Cpt(SignalWithRBV, 'TEC')
-    tec_temp = Cpt(SignalWithRBV, 'TEC_TEMP')
-    curr_tec_temp = Cpt(EpicsSignalRO, 'CURR_TEC_TEMP_RBV') 
+    
+    # Thermal electric cooler
+    tec_device = Cpt(QEProTEC, '')
     
     # Light source feature signals
     light_source = Cpt(SignalWithRBV, 'LIGHT_SOURCE')
@@ -61,7 +72,7 @@ class QEPro(Device):
     # Integration time settings (in ms)
     int_min_time = Cpt(EpicsSignalRO, 'INT_MIN_TIME_RBV')
     int_max_time = Cpt(EpicsSignalRO, 'INT_MAX_TIME_RBV')
-    integration_time = Cpt(SignalWithRBV, 'INTEGRATION_TIME')
+    integration_time = Cpt(SignalWithRBV, 'INTEGRATION_TIME', kind='normal')
     
     # Internal buffer feature settings
     buff_min_capacity = Cpt(EpicsSignalRO, 'BUFF_MIN_CAPACITY_RBV')
@@ -70,9 +81,9 @@ class QEPro(Device):
     buff_element_count = Cpt(EpicsSignalRO, 'BUFF_ELEMENT_COUNT_RBV')
 
     # Formatted Spectra
-    spectrum = Cpt(EpicsSignal, 'SPECTRUM')
-    dark = Cpt(EpicsSignal, 'DARK')
-    reference = Cpt(EpicsSignal, 'REFERENCE')
+    spectrum = Cpt(EpicsSignal, 'SPECTRUM', kind='normal')
+    dark = Cpt(EpicsSignal, 'DARK', kind='normal')
+    reference = Cpt(EpicsSignal, 'REFERENCE', kind='normal')
     
     # Length of spectrum (in pixels)
     formatted_spectrum_len = Cpt(EpicsSignalRO, 'FORMATTED_SPECTRUM_LEN_RBV')
@@ -86,14 +97,12 @@ class QEPro(Device):
     ref_available = Cpt(EpicsSignalRO, 'REF_AVAILABLE_RBV')
 
     # Collection settings and start signals.
-    collect = Cpt(SignalWithRBV, 'COLLECT')
+    acquire = Cpt(SignalWithRBV, 'COLLECT', put_complete=True)
     collect_mode = Cpt(SignalWithRBV, 'COLLECT_MODE')
     spectrum_type = Cpt(SignalWithRBV, 'SPECTRUM_TYPE')
     correction = Cpt(SignalWithRBV, 'CORRECTION')
     trigger_mode = Cpt(SignalWithRBV, 'TRIGGER_MODE')
 
-
-    def __init__(self, prefix, *, )
 
     @property
     def has_nlc_feature(self):
@@ -120,16 +129,38 @@ class QEPro(Device):
         return self.features.get() & 1
 
 
+    def set_temp(self, temperature):
+        self.tec_device.set(temperature).wait()
+
+
     def get_dark_frame(self):
 
         self.spectrum_type.put(1)
-        self.collect.put(1)
+        self.acquire.put(1, wait=True)
     
     def get_reference_frame(self):
 
         if self.dark_available.get() == 0:
             return
         self.spectrum_type.put(2)
-        self.collect.put(1)
+        self.acquire.put(1, wait=True)
+
+
+    def setup_collection(self, integration_time, num_spectra_to_average, correction_type='reference', electric_dark_correction=True):
+        self.integration_time.put(integration_time)
+        self.num_spectra = num_spectra_to_average
+        if electric_dark_correction:
+            self.electric_dark_correction = True
+
+
+        self.get_dark_frame()
+        self.get_reference_frame()
+
+
+    def trigger(self):
+
+        self.acquire.put(1, wait=True)
+
+    
 
 
