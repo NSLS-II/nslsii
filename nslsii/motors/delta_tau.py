7de4d41b4e931afef348ac99d9ff769f6ab1bfb6
maffettone <pmaffetto@bnl.gov>
2024-01-03 09:58:47 -0800

    feat: add pmac kill signal and switch

---
+++ nslsii/motors/delta_tau.py
@@ -0,0 +1,42 @@
+"""
+Ophyd devices and signals for Delta Tau devices, such as Programmable Multi-Axis Controller (PMAC-PC)
+"""
+
+import logging
+
+from ophyd import Component as Cpt
+from ophyd import Device, EpicsSignal, EpicsSignalRO, Kind
+from ophyd.utils.epics_pvs import raise_if_disconnected
+
+
+class PMACStatus(EpicsSignalRO):
+    """
+    A signal to read the status of the PMAC-PC controller by checking the bit 11 of the status register.
+    """
+
+    @raise_if_disconnected
+    def read(self):
+        value = self.get() >> 11 & 1 == 0
+        return {self.name: {"value": value, "timestamp": self.timestamp}}
+
+
+class PMACKillSwitch(Device):
+    """
+    A device to kill the PMAC-PC controller.
+    In the named version of the EPICS record, the status is expected at Sts:4-Sts.
+    Whereas in the axis numbered version, the status is expected at Sts:1-Sts.
+    """
+
+    kill = Cpt(EpicsSignal, "Cmd:Kill-Cmd.PROC", kind=Kind.omitted)
+    status = Cpt(PMACStatus, "Sts:4-Sts", kind=Kind.normal)
+
+    def set(self, value, *args, **kwargs):
+        """
+        Set the kill switch to the given value
+        """
+        if value != 1:
+            logging.getLogger(__name__).warning(
+                "The value of the PMACKiller should only ever be set to 1. " "Changing the setpoint to 1 now."
+            )
+            value = 1
+        self.kill.set(value, *args, **kwargs)
