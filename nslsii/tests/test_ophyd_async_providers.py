03464727a16e6edb0ae32b6806ca865c0e06451e
Jakub Wlodek <jwlodek@bnl.gov>
2024-09-05 12:08:57 -0400

    Tests for filename providers

--- nslsii/tests/test_ophyd_async_providers.py
+++ nslsii/tests/test_ophyd_async_providers.py
@@ -2,11 +2,18 @@ import pytest
 import os
 from ophyd_async.core import StaticFilenameProvider
 
-from nslsii.ophydv2 import ProposalNumYMDPathProvider, ProposalNumScanNumPathProvider, ShortUUIDFilenameProvider, DeviceNameFilenameProvider
+from nslsii.ophydv2 import (
+    ProposalNumYMDPathProvider,
+    ProposalNumScanNumPathProvider,
+    ShortUUIDFilenameProvider,
+    DeviceNameFilenameProvider,
+)
+
 
 @pytest.fixture
 def fp():
     return StaticFilenameProvider("test")
+
 
 @pytest.fixture
 def dummy_re_md_dict():
@@ -25,23 +32,30 @@ def dummy_re_md_dict():
         (False, False, None),
     ],
 )
-def test_proposal_num_ymd_path_provider(dev_name_base, use_dev_name, expected_devname_pos, fp, dummy_re_md_dict):
+def test_proposal_num_ymd_path_provider(
+    dev_name_base, use_dev_name, expected_devname_pos, fp, dummy_re_md_dict
+):
     os.environ["BEAMLINE_ACRONYM"] = "tst"
 
-    pp = ProposalNumYMDPathProvider(fp, dummy_re_md_dict, device_name_as_base_dir=dev_name_base)
+    pp = ProposalNumYMDPathProvider(
+        fp, dummy_re_md_dict, device_name_as_base_dir=dev_name_base
+    )
 
     if use_dev_name:
         info = pp(device_name="test")
     else:
         info = pp()
 
-    assert str(info.directory_path).startswith("/nsls2/data/tst/proposals/2024-3/pass-000000/assets")
+    assert str(info.directory_path).startswith(
+        "/nsls2/data/tst/proposals/2024-3/pass-000000/assets"
+    )
     assert info.create_dir_depth == -4
 
     if use_dev_name:
-        str(info.directory_path).split('/')[expected_devname_pos] == "test"
+        str(info.directory_path).split("/")[expected_devname_pos] == "test"
     else:
         assert "test" not in str(info.directory_path)
+
 
 def test_proposal_num_scan_num_path_provider(fp, dummy_re_md_dict):
     os.environ["BEAMLINE_ACRONYM"] = "tst"
@@ -50,10 +64,33 @@ def test_proposal_num_scan_num_path_provider(fp, dummy_re_md_dict):
 
     info = pp()
 
-    assert str(info.directory_path) == "/nsls2/data/tst/proposals/2024-3/pass-000000/assets/scan_00000"
+    assert (
+        str(info.directory_path)
+        == "/nsls2/data/tst/proposals/2024-3/pass-000000/assets/scan_00000"
+    )
     assert info.create_dir_depth == -1
 
     info_b = pp()
 
-    assert str(info_b.directory_path) == "/nsls2/data/tst/proposals/2024-3/pass-000000/assets/scan_00001"
+    assert (
+        str(info_b.directory_path)
+        == "/nsls2/data/tst/proposals/2024-3/pass-000000/assets/scan_00001"
+    )
     assert info_b.create_dir_depth == -1
+
+
+def test_device_name_filename_provider():
+    dev_name_fp = DeviceNameFilenameProvider()
+    assert "test" == dev_name_fp(device_name="test")
+
+    with pytest.raises(RuntimeError):
+        dev_name_fp()
+
+
+def test_short_uuid_filename_provider():
+    sid_fp = ShortUUIDFilenameProvider()
+
+    filename = sid_fp(device_name="test")
+    assert filename.startswith("test")
+    assert "_" in filename
+    assert len(filename.split("_")[-1]) == 22
